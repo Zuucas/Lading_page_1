36c8c31d7c086e5700b8ecfade5dc47ae5428ebc
Lucas Melo <lucasmelo402@gmail.com>
2024-04-10 01:11:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Troca celular
+function imgTroca(e){
+    document.querySelector('#phone').src = e;
+}
+function circleTroca(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
